773e11671b309d6dde573755ef31d99efe719869
onukwufor dominic <dominicrotimi@gmail.com>
2024-07-12 15:29:10 +0100

    Chabges made

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import JoshiversePage from "./pages/joshiverse/JoshiversePage";
+import MakokoOutreach from "./pages/sasi/MakokoOutreach";
+import PercilOutreach from "./pages/sasi/PercilOutreach";
 import SasiPage from "./pages/sasi/SasiPage";
+import WesleyOutreach from "./pages/sasi/WesleyOutreach";
 
 export const ROUTES = [
     {
@@ -9,5 +12,17 @@ export const ROUTES = [
     {
         path: '/sasi',
         element: <SasiPage />
-    }
+    },
+    {
+        path: '/our-work/emergency-response',
+        element: <MakokoOutreach />
+      },
+      {
+        path: '/our-work/child-poverty',
+        element: <PercilOutreach />
+      },
+      {
+        path: '/our-work/child-protection',
+        element: <WesleyOutreach />
+      }
 ]

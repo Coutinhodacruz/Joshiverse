a63dab3d5a8cfa3e50187a2b6ab99c3a17dba684
onukwufor dominic <dominicrotimi@gmail.com>
2024-07-19 23:54:27 +0100

    Changes Made

--- src/pages/sasi/SasiPage.jsx
+++ src/pages/sasi/SasiPage.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeroPage from './HeroPage'
 import About from './About'
 import Event from './Event'
 import Contact from './Contact'
+import ScrollToTop from '../../scripts/ScrollToTop'
 
 const SasiPage = () => {
+
   return (
     <div className='bg-[#eeeeee]'>
+      <ScrollToTop/>
         <HeroPage/>
         <About/>
        <Event/>
